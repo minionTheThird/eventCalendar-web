92739fd4f86cad6d50e50b35dee5c206ff45ff75
minionTheThird <sanjaysingh.mt3@gmail.com>
2015-10-14 21:49:27 +0530

    Added 1 week look ahead js code for hackerEarth calendar

--- js/main.js
+++ js/main.js
@@ -37,8 +37,8 @@ function getCodeforcesEvents()
             },
             jsonpCallback: 'callback',
             type: 'GET',
-            success: function (data1) {
-                var events = data1.result;
+            success: function (dataCF) {
+                var events = dataCF.result;
                 var table = document.getElementById("codeforces");
 
                 for(var id in events){
@@ -85,26 +85,65 @@ function getCodeforcesEvents()
 
 function getHackerEarthEvents()
 {
+        console.log("inside HE");
         var inputHTML = "";
         var calendarId = 'hackerearth.com_73f0o8kl62rb5v1htv19p607e4@group.calendar.google.com'
         var startTime = new Date();
         var endTime = new Date();
+        var key = 'AIzaSyCAkVQVwMzmPHxbaLUAqvb6dYUwjKU5qnM';
+        //usage limit to http://minionthethird.github.io/*
+        //working = AIzaSyCAkVQVwMzmPHxbaLUAqvb6dYUwjKU5qnM
+        //other key = AIzaSyBQal2rNhP5SRkU5hZytY7Yb8nYc5Q1nrc
         endTime.setDate(endTime.getDate()+7);
-        var URL = 'https://www.googleapis.com/calendar/v3/calendars/'+calendarId+'/events?timeMax='+endTime.toISOString()+'&timeMin='+startTime.toISOString();
-        console.log(calendarId);
-        console.log(startTime);
-        console.log(endTime);
-        console.log(URL);
+        var URL = 'https://www.googleapis.com/calendar/v3/calendars/'+calendarId+'/events?key='+key+'&timeMax='+endTime.toISOString()+'&timeMin='+startTime.toISOString();
         $.ajax({
             url: URL,
             dataType: 'JSONP',
-            data : {
-                jsonp:"callback",
-            },
-            jsonpCallback: 'callback',
             type: 'GET',
-            success: function (data1) {
-                document.getElementById("hackerEarth").innerHTML = JSON.stringify(data1);
+            success: function (dataHE) {
+              console.log("inside he success")
+                var events = dataHE.items;
+                events.sort(function(a, b) {
+                    return b.start.dateTime.localeCompare(a.start.dateTime);
+                });
+
+                var table = document.getElementById("hackerEarth");
+
+                for(var id in events){
+                  if(events[id].status == "confirmed"){
+
+                    var durationMinutes = 0;
+                    var durationHours = 0;
+
+                    var start_date = new Date(events[id].start.dateTime);
+                    var end_date = new Date(events[id].end.dateTime);
+                    start_date.format();
+                    end_date.format();
+
+                    var row = table.insertRow(1);
+                    var l_name = row.insertCell(0);
+                    var l_sdate = row.insertCell(1);
+                    var l_edate = row.insertCell(2);
+                    var l_calendar = row.insertCell(3);
+
+                    l_name.innerHTML = events[id].summary;
+                    l_sdate.innerHTML = start_date.DATE;//start_date.toLocaleString();
+                    l_edate.innerHTML = end_date.DATE;
+                    //l_duration.innerHTML = durationHours +"h "+durationMinutes+"m";
+
+                    var template = $('#calendar-template').html();
+                    Mustache.parse(template);   // optional, speeds up future uses
+                    var rendered = Mustache.render(
+                                      template,
+                                      {
+                                        name : events[id].summary,
+                                        startDateTime : start_date.dateTime ,
+                                        endDateTime : end_date.dateTime
+                                      });
+                    l_calendar.innerHTML = rendered;
+
+                  }
+                 };
             },
             error: function (xhr, ajaxOptions, thrownError) {
                 console.log("error for : " + this.handle)
