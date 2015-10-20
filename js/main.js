a448a18b5ed8cb06dbeccab5fb95ef872e6ce04a
minionTheThird <sanjaysingh.mt3@gmail.com>
2015-10-20 10:45:49 +0530

    Refactored Codebase

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(
     function()
     {
-        getCodeforcesEvents();
-        getCodeChefEvents();
-        getHackerEarthEvents();
+        getEvents('CodeForces');
+        getEvents('HackerEarth');
+        getEvents('CodeChef');
     }
 );
 
@@ -26,193 +26,119 @@ Date.prototype.format = function() {
     this.DATE = this.DAY+", "+this.MMM+" "+this.DD+" "+this.YYYY+", "+this.TIME+" "+this.timeZone;
 
 }
-
-function getCodeforcesEvents()
-{
-        var inputHTML = "";
-        $.ajax({
-            url: 'http://codeforces.com/api/contest.list',
-            dataType: 'JSONP',
-            data : {
-                jsonp:"callback",
-            },
-            jsonpCallback: 'callback',
-            type: 'GET',
-            success: function (dataCF) {
-                var events = dataCF.result;
-                var table = document.getElementById("codeforces");
-
-                for(var id in events){
-                  if(events[id].phase != "FINISHED"){
-
-                    var durationMinutes = (events[id].durationSeconds%3600)/60;
-                    var durationHours = (events[id].durationSeconds-durationMinutes*60)/3600;
-
-                    var start_date = new Date(events[id].startTimeSeconds*1000);
-                    var end_date = new Date((events[id].startTimeSeconds + events[id].durationSeconds)*1000);
-                    start_date.format();
-                    end_date.format();
-
-                    var row = table.insertRow(1);
-                    var l_name = row.insertCell(0);
-                    var l_date = row.insertCell(1);
-                    var l_duration = row.insertCell(2);
-                    var l_calendar = row.insertCell(3);
-
-                    l_name.innerHTML = events[id].name;
-                    l_date.innerHTML = start_date.DATE;//start_date.toLocaleString();
-                    l_duration.innerHTML = durationHours +"h "+durationMinutes+"m";
-
-                    var template = $('#calendar-template').html();
-                    Mustache.parse(template);   // optional, speeds up future uses
-                    var rendered = Mustache.render(
-                                      template,
-                                      {
-                                        name : events[id].name,
-                                        startDateTime : start_date.dateTime ,
-                                        endDateTime : end_date.dateTime
-                                      });
-                    l_calendar.innerHTML = rendered;
-
-                  }
-                 };
-            },
-            error: function (xhr, ajaxOptions, thrownError) {
-                console.log("error for : " + this.handle)
-            }
-
-        });
-}
-
-function getHackerEarthEvents()
-{
-        var inputHTML = "";
-        var calendarId = 'hackerearth.com_73f0o8kl62rb5v1htv19p607e4@group.calendar.google.com'
-        var startTime = new Date();
-        var endTime = new Date();
-        var key = 'AIzaSyCAkVQVwMzmPHxbaLUAqvb6dYUwjKU5qnM';
         //usage limit to http://minionthethird.github.io/*
         //working = AIzaSyCAkVQVwMzmPHxbaLUAqvb6dYUwjKU5qnM
         //other key = AIzaSyBQal2rNhP5SRkU5hZytY7Yb8nYc5Q1nrc
-        endTime.setDate(endTime.getDate()+7);
-        var URL = 'https://www.googleapis.com/calendar/v3/calendars/'+calendarId+'/events?key='+key+'&timeMax='+endTime.toISOString()+'&timeMin='+startTime.toISOString();
-        $.ajax({
-            url: URL,
-            dataType: 'JSONP',
-            type: 'GET',
-            success: function (dataHE) {
-                var events = dataHE.items;
-                events.sort(function(a, b) {
-                    return b.start.dateTime.localeCompare(a.start.dateTime);
-                });
-                var table = document.getElementById("hackerEarth");
 
-                for(var id in events){
-                  if(events[id].status == "confirmed"){
-
-                    var durationMinutes = 0;
-                    var durationHours = 0;
-
-                    var start_date = new Date(events[id].start.dateTime);
-                    var end_date = new Date(events[id].end.dateTime);
-                    start_date.format();
-                    end_date.format();
-
-                    var row = table.insertRow(1);
-                    var l_name = row.insertCell(0);
-                    var l_sdate = row.insertCell(1);
-                    var l_edate = row.insertCell(2);
-                    var l_calendar = row.insertCell(3);
-
-                    l_name.innerHTML = events[id].summary;
-                    l_sdate.innerHTML = start_date.DATE;//start_date.toLocaleString();
-                    l_edate.innerHTML = end_date.DATE;
-                    //l_duration.innerHTML = durationHours +"h "+durationMinutes+"m";
-
-                    var template = $('#calendar-template').html();
-                    Mustache.parse(template);   // optional, speeds up future uses
-                    var rendered = Mustache.render(
-                                      template,
-                                      {
-                                        name : events[id].summary,
-                                        startDateTime : start_date.dateTime ,
-                                        endDateTime : end_date.dateTime
-                                      });
-                    l_calendar.innerHTML = rendered;
-
-                  }
-                 };
-            },
-            error: function (xhr, ajaxOptions, thrownError) {
-                console.log("error for : " + this.handle)
-            }
-
-        });
+function getEvents(calendarName){
+  var URL = prepareURLfor(calendarName);
+  makeAjaxRequest(calendarName, URL);
 }
 
-function getCodeChefEvents()
-{
-        var inputHTML = "";
-        var calendarId = 'codechef.com_3ilksfmv45aqr3at9ckm95td5g%40group.calendar.google.com'
-        var startTime = new Date();
-        var endTime = new Date();
-        var key = 'AIzaSyCAkVQVwMzmPHxbaLUAqvb6dYUwjKU5qnM';
-        //usage limit to http://minionthethird.github.io/*
-        //working = AIzaSyCAkVQVwMzmPHxbaLUAqvb6dYUwjKU5qnM
-        //other key = AIzaSyBQal2rNhP5SRkU5hZytY7Yb8nYc5Q1nrc
-        endTime.setDate(endTime.getDate()+14);
-        var URL = 'https://www.googleapis.com/calendar/v3/calendars/'+calendarId+'/events?key='+key+'&timeMax='+endTime.toISOString()+'&timeMin='+startTime.toISOString();
-        $.ajax({
-            url: URL,
-            dataType: 'JSONP',
-            type: 'GET',
-            success: function (data) {
-              var events = data.items;
-              events.sort(function(a, b) {
-                  return b.start.dateTime.localeCompare(a.start.dateTime);
-              });
-              console.log(JSON.stringify(events));
-              var table = document.getElementById("codeChef");
-
-              for(var id in events){
-                if(events[id].status == "confirmed"){
-
-                  var durationMinutes = 0;
-                  var durationHours = 0;
-
-                  var start_date = new Date(events[id].start.dateTime);
-                  var end_date = new Date(events[id].end.dateTime);
-                  start_date.format();
-                  end_date.format();
-
-                  var row = table.insertRow(1);
-                  var l_name = row.insertCell(0);
-                  var l_sdate = row.insertCell(1);
-                  var l_edate = row.insertCell(2);
-                  var l_calendar = row.insertCell(3);
-
-                  l_name.innerHTML = events[id].summary;
-                  l_sdate.innerHTML = start_date.DATE;//start_date.toLocaleString();
-                  l_edate.innerHTML = end_date.DATE;
-                  //l_duration.innerHTML = durationHours +"h "+durationMinutes+"m";
-
-                  var template = $('#calendar-template').html();
-                  Mustache.parse(template);   // optional, speeds up future uses
-                  var rendered = Mustache.render(
-                                    template,
-                                    {
-                                      name : events[id].summary,
-                                      startDateTime : start_date.dateTime ,
-                                      endDateTime : end_date.dateTime
-                                    });
-                  l_calendar.innerHTML = rendered;
-
-                }
-               };
-            },
-            error: function (xhr, ajaxOptions, thrownError) {
-                console.log("error for : " + this.handle)
-            }
-
-        });
+function prepareURLfor(calendarName){
+  var URL;
+  if(calendarName == 'CodeForces')
+    URL = 'http://codeforces.com/api/contest.list';
+  else{
+    var calendarId;
+    var key = 'AIzaSyCAkVQVwMzmPHxbaLUAqvb6dYUwjKU5qnM';
+    var lookAhead;
+    if (calendarName == 'HackerEarth'){
+      calendarId = 'hackerearth.com_73f0o8kl62rb5v1htv19p607e4@group.calendar.google.com';
+      lookAhead = 7; // 7 days look ahead
+    }
+    else{
+      calendarId = 'codechef.com_3ilksfmv45aqr3at9ckm95td5g%40group.calendar.google.com'
+      lookAhead = 14; // 14 days look ahead
+    }
+    var startTime = new Date();
+    var endTime = new Date();
+    endTime.setDate(endTime.getDate()+lookAhead);
+    URL = 'https://www.googleapis.com/calendar/v3/calendars/'+calendarId+'/events?key='+key+'&timeMax='+endTime.toISOString()+'&timeMin='+startTime.toISOString();
+  }
+  return URL;
 }
+
+function makeAjaxRequest(calendarName, URL){
+  $.ajax({
+      url: URL,
+      dataType: 'JSONP',
+      data : {
+          jsonp:"callback",
+      },
+      jsonpCallback: 'callback',
+      type: 'GET',
+      success: function(data){
+          processEvents(data, calendarName);
+      },
+      error: function (xhr, ajaxOptions, thrownError) {
+          console.log("error for : " + this.handle)
+      }
+  });
+};
+
+function processEvents(calendarEvents, calendarName){
+    var events;
+    var table = document.getElementById(calendarName);
+    if(calendarName == 'CodeForces'){
+        events = calendarEvents.result;
+        for(var id in events){
+          if(events[id].phase != "FINISHED"){
+            var row = table.insertRow(1);
+            var eventDetail = formatEventDetail(events[id], calendarName);
+            row.innerHTML = applyCalendarTemplate(eventDetail);
+          }
+        }
+    } else {
+      events = calendarEvents.items;
+      events.sort(function(a, b) {
+          return b.start.dateTime.localeCompare(a.start.dateTime);
+      });
+      for(var id in events){
+        if(events[id].status == "confirmed"){
+          var row = table.insertRow(1);
+          var eventDetail = formatEventDetail(events[id], calendarName);
+          row.innerHTML = applyCalendarTemplate(eventDetail);
+        }
+      }
+    }
+};
+
+function formatEventDetail(eventData, calendarName){
+  var eventName;
+  var eventStartTime;
+  var eventEndTime;
+  if(calendarName == 'CodeForces'){
+    eventName = eventData.name;
+    eventStartTime = new Date(eventData.startTimeSeconds*1000);
+    eventEndTime = new Date((eventData.startTimeSeconds + eventData.durationSeconds)*1000);
+  }
+  else{
+    eventName = eventData.summary;
+    eventStartTime = new Date(eventData.start.dateTime);
+    eventEndTime = new Date(eventData.end.dateTime);
+  }
+  eventStartTime.format();
+  eventEndTime.format();
+  var eventDetail = {
+    name : eventName,
+    startDate : eventStartTime,
+    endDate : eventEndTime
+  }
+  return eventDetail;
+};
+
+function applyCalendarTemplate(eventDetail){
+  var template = $('#calendar-template').html();
+  Mustache.parse(template);   // optional, speeds up future uses
+  var rendered = Mustache.render(
+                    template,
+                    {
+                      name : eventDetail.name,
+                      startDate : eventDetail.startDate.DATE,
+                      endDate : eventDetail.endDate.DATE,
+                      startDateTime : eventDetail.startDate.dateTime,
+                      endDateTime : eventDetail.endDate.dateTime
+                    });
+  return rendered;
+};
